1ddc61d46303b1b94067951e30d30aff7c873db1
digaso <diogomoura2014@gmail.com>
2023-01-13 18:24:28 +0000

    Added building class

---
+++ src/building.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class building
+{
+  uint id;
+  string name;
+  uint size_workers;
+public:
+  ~building() {};
+  building() {};
+  building(uint id, string name, uint size_workers) {
+    this->id = id;
+    this->name = name;
+    this->size_workers = size_workers;
+  }
+};
+
